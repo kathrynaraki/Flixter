c497380813cb6bc8e523ca5ad480afbb7474def6
Kathryn Araki <arakik@fb.com>
2017-06-22 16:49:38 -0700

    Initial commit

---
+++ app/src/main/java/com/codepath/flixter/models/Config.java
@@ -0,0 +1,38 @@
+package com.codepath.flixter.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by arakik on 6/22/17.
+ */
+
+public class Config {
+    // the base url for loading images
+    String imageBaseUrl;
+    // the poster size to use when getting images
+    String posterSize;
+
+    public Config(JSONObject object) throws JSONException {
+        JSONObject images = object.getJSONObject("images");
+        imageBaseUrl = images.getString("secure_base_url");
+
+        // get the poster size
+        JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
+        // use the option at index 3, or w342 as a fallback
+        posterSize = posterSizeOptions.optString(3, "w342");
+    }
+
+    // helper method for creating urls
+    public String getImageUrl(String size, String path) {
+        return String.format("%s%s%s", imageBaseUrl, size, path);
+    }
+    public String getImageBaseUrl() {
+        return imageBaseUrl;
+    }
+
+    public String getPosterSize() {
+        return posterSize;
+    }
+}
